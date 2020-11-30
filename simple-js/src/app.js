c52b8ad3736dfb23d5674ddb923b6c1aee1525e3
javibc1 <javibc1@hotmail.com>
2020-11-30 18:05:49 +0100

    new version

--- simple-js/src/app.js
+++ simple-js/src/app.js
@@ -6,7 +6,7 @@ let server = http.createServer(function(req,res){
     'content-type':'text/html'
   });
   
-  res.write('<h1 style="color: blue;">Hello World from Tekton Events Triggers new try</h1>');
+  res.write('<h1 style="color: blue;">Hello World from Tekton in Santalucia</h1>');
   res.end();
 });
 
